2fb755916946f86e547c04249930f33f7d18feec
郭启帆 <1312442746@qq.com>
2019-08-27 15:34:50 +0800

    misc:test

--- demo/src/main/java/com/github/yeecode/objectlogger/demo/ObjectLoggerDemoApplication.java
+++ demo/src/main/java/com/github/yeecode/objectlogger/demo/ObjectLoggerDemoApplication.java
@@ -24,5 +24,6 @@ public class ObjectLoggerDemoApplication {
         System.out.println("http://127.0.0.1:12302/ObjectLoggerDemo/task/start");
         System.out.println("http://127.0.0.1:12302/ObjectLoggerDemo/task/update");
         System.out.println("###############################");
+        System.out.println("###############################---------------------");
     }
 }
